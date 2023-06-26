cab96ff862c32ed7ec41ef7b8648fd7397b1cff0
hardope <adeyeriopeoluwa05@gmail.com>
2023-06-26 08:29:19 -0700

    1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,7 +3,7 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    const task = true;
+    const task = true; // eslint-disable-line no-unused-vars
     const task2 = false;
   }
 
