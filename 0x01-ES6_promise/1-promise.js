27c65c66eedae26f3bb4f5c46111bed889bb15dc
hardope <adeyeriopeoluwa05@gmail.com>
2023-06-27 07:32:36 -0700

    Mode

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -7,7 +7,7 @@ export default function getResponseFromAPI(success) {
         body: 'success',
       });
     } else {
-      reject(Error('The fake API is not working currently'));
+      reject(new Error('The fake API is not working currently'));
     }
   });
 }
