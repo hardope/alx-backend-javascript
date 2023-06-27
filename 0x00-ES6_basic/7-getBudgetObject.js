8b0bb6b7f982baeca0c4cedb71cfc99cf72cd294
hardope <adeyeriopeoluwa05@gmail.com>
2023-06-27 00:20:26 -0700

    Corrections

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,5 +1,10 @@
 export default function getBudgetObject(income, gdp, capita) {
-  const budget = { income, gdp, capita };
+  const budget = {
+    income,
+    gdp,
+    capita,
+  };
 
   return budget;
 }
+
